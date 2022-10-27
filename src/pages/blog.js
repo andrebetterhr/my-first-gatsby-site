af0b9e8b101eee5666f782cbef0207b9b9ced964
andrebetterhr <andre.schaeffer@betterhr.de>
2022-10-27 16:27:33 +0200

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,24 +8,29 @@ const pageTitle = 'My Blog Posts'
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle={pageTitle}>
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map(node => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>{node.excerpt}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql`
     query {
-        allFile {
+        allMdx(sort: {order: DESC, fields: frontmatter___date}) {
             nodes {
-                name
+              frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                title
+              }
+              id
+              excerpt
             }
         }
     }
